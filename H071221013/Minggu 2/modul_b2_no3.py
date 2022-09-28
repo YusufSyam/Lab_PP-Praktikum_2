e3a940b6ce5bf94cc5f43d14ef056d7bb137eacd
Susantii92 <susantii9261@gmail.com>
2022-09-28 11:05:43 +0700

    Push Kedua

---
+++ H071221013/Minggu 2/modul_b2_no3.py
@@ -0,0 +1,12 @@
+a=int(input('a = '))
+b=int(input('b = '))
+c=int(input('c = '))
+
+if a>=b and a>=c :
+    print('>', a, 'adalah nilai terbesar')
+elif b>=a and b>=c :
+     print('>', b, 'adalah nilai terbesar')
+elif c>=a and c>=b :
+     print('>', c, 'adalah nilai terbesar')
+
+
