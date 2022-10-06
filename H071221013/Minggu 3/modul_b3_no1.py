ec5437f04d5015e51b91c66a34ebe6054d18e946
Susantii92 <susantii9261@gmail.com>
2022-10-06 07:26:15 +0700

    Kumpul Tugas 3

---
+++ H071221013/Minggu 3/modul_b3_no1.py
@@ -0,0 +1,17 @@
+x = int(input("Nilai x : "))
+y = int(input("Nilai y : "))
+angka = 1
+
+# X<Y
+
+while angka <= y :
+    for i in range(x):
+        if angka<=y :
+            print(angka, end = " ")
+        angka = angka + 1
+    print()
+
+        
+
+    
+
