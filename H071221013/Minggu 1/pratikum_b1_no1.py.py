d0e55e32154b834a02d87b3ff67985bb7f2b6fd0
Yusuf Syam <muhyusufsyamsyam@gmail.com>
2022-10-07 15:14:22 +0800

    Delete H071221013 directory

--- H071221013/Minggu 1/pratikum_b1_no1.py.py
+++
@@ -1,5 +0,0 @@
-a = "45"
-b = 50
-a = int(a)
-result = a+b
-print(result)
--- H071221013/Minggu 3/pratikum_b3.py
+++
@@ -1,4 +0,0 @@
-t = int(input("Masukkan Tinggi Segitiga: "))
-for i in range(1, t+1):
-    print((t-i) * "  " + (i * "* ") + ((i-1) * "* "))
-    
